243c3dce903c1112c301bd6393d88131ccf26196
chaeyoung <70706563+Cheyoung0203@users.noreply.github.com>
2023-12-30 16:08:39 +0900

    effects part intro

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
-import Button from "./Button";
-import styles from "./App.module.css";
+import { useState } from "react";
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const onClick = () => setValue((prev) => prev + 1);
+  console.log("call an api");
   return (
     <div>
-      <h1 className={styles.title}>Welcome back!!!</h1>
-      <Button text={"Countinue"}></Button>
+      <h1>{counter}</h1>
+      <button onClick={onClick}>click me</button>
     </div>
   );
 }
 
 export default App;
+
+/**
+ * state를 변경할 때 모든 code들은 항상 다시 실행된다.
+ *
+ * 하지만 component 내부의 몇몇 코드는 처음 rendering될 때만 코드가 실행되길 원할 수 있다.
+ * example
+ * API를 통해 데이터를 가져올 때
+ */
